d24fe2f6ae33e81f8c830fb945af02631900afaa
bentech3 <bentech3>
2025-12-10 10:29:00 -0800

    Initial gh-pages hosting

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    base: '/Medikart-ug/'
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
